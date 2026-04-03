1f0c631132370857c3cbbff0da7d79844254a6e5
MariamRazmadze <mariam.razmadze.1@iliauni.edu.ge>
2026-04-03 20:44:49 +0400

    add static components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,58 @@
-function App() {
-  return <main>Game goes here</main>;
-}
+import { useState } from "react";
+import { languages } from "./languages";
 
-export default App;
+export default function AssemblyEndgame() {
+  const [currentWord, setCurrentWord] = useState("react");
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  console.log(guessedLetters);
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  function addGuessedLetter(letter: string) {
+    setGuessedLetters((prevLetters) =>
+      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter],
+    );
+  }
+
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    return (
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
+  });
+
+  const letterElements = currentWord
+    .split("")
+    .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+
+  const keyboardElements = alphabet.split("").map((letter) => (
+    <button key={letter} onClick={() => addGuessedLetter(letter)}>
+      {letter.toUpperCase()}
+    </button>
+  ));
+
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New Game</button>
+    </main>
+  );
+}
